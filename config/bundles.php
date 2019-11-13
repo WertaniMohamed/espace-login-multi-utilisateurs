fdcacaaa06d5e9c49a70629b591a7a0dc42ab9f6
WertaniMohamed <mohamedwertani1@gmail.com>
2019-11-13 17:12:30 +0100

    install composer require twig/extensions

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
