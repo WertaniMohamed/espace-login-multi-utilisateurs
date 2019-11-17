66302704f2e6510be3ee524cce27811cf38bdbd7
Mohamed wertani <mohamedwertani1@gmail.com>
2019-11-17 22:23:50 +0100

    Revert "Home -  Ajouter liens pour lem menu topBar"
    This reverts commit 1d799388

--- config/bundles.php
+++ config/bundles.php
@@ -13,5 +13,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
